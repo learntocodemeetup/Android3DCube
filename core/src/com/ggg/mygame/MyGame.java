adbca44f7460be8a66540cb65b52879097c94148
cyber2024 <russell.elfenbein@gmail.com>
2015-08-18 20:45:38 +0800

    Changed model to space ship

--- core/src/com/ggg/mygame/MyGame.java
+++ core/src/com/ggg/mygame/MyGame.java
@@ -2,19 +2,17 @@ package com.ggg.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class MyGame extends ApplicationAdapter {
 	public PerspectiveCamera cam;
@@ -47,10 +45,8 @@ public class MyGame extends ApplicationAdapter {
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 
-		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createBox(5f, 5f, 5f,
-				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		ModelLoader loader = new ObjLoader();
+		model = loader.loadModel(Gdx.files.internal("ship.obj"));
 		instance = new ModelInstance(model);
 	}
 
